e83c6d95cbe9a6017e32e5020c990821c6567bab
Matthew Newell <MatthewKNewell@gmail.com>
2019-03-12 00:58:06 -0700

    implement cards to featured work

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-// Copyright 2018, Google LLC.
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 'use strict';
 
@@ -21,7 +9,6 @@ var path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  //res.setHeader('content-type', 'text/hmtl');
   res.statusCode = 202;
   res.sendFile(path.join(__dirname + '/public/views/index.html'));
 });
@@ -35,13 +22,10 @@ app.get('/about', (req, res) => {
 });
 
 if (module === require.main) {
-  // [START server]
-  // Start the server
   const server = app.listen(process.env.PORT || 8080, () => {
     const port = server.address().port;
     console.log(`App listening on port ${port}`);
   });
-  // [END server]
 }
 
 module.exports = app;
