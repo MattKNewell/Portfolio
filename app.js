c0003967108be05a62ce1970d65e4b1b8197516e
Matthew Newell <mattknewell@gmail.com>
2019-12-21 12:42:41 -0800

    add new resume and new landing page

--- app.js
+++ app.js
@@ -13,9 +13,19 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/public/views/index.html'));
 });
 
+app.get('/videography', (req, res) => {
+  res.statusCode = 202;
+  res.sendFile(path.join(__dirname + '/public/views/videography.html'));
+});
+
 app.get('/resume', (req, res) => {
   res.sendFile(path.join(__dirname + '/public/views/Resume.html'));
 });
+
+app.get('/tinkering', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/views/tinkering.html'));
+});
+
 /*
 app.get('/wedding-packages', (req, res) => {
   res.sendFile(path.join(__dirname + '/public/views/wedding-packages.html'));
